bbdff84f89d68552b126d97475cf56dea49a6cff
ridge MM <juniorridge93@yahoo.fr>
2020-05-12 11:13:02 +0200

    inscription ok

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -31,7 +31,7 @@ BoolAutres : boolean = false;
   ngOnInit(): void {
     this.formUser=new FormGroup({
       privilege: new FormControl(),
-       idUser : new FormControl(),
+      // idUser : new FormControl(),
       prenom: new FormControl(),
       nom: new FormControl(),
       login: new FormControl(),
@@ -40,106 +40,46 @@ BoolAutres : boolean = false;
       rue: new FormControl(),
       ville: new FormControl(),
       département: new FormControl(),
-    producteur :new FormGroup({
-      raisonSociale: new FormControl(),
-    siret: new FormControl(),
-    artisant :new FormGroup({
-      raisonSociale: new FormControl(),
-    siret: new FormControl(),
-    commercant :new FormGroup({
-      raisonSociale: new FormControl(),
-    siret: new FormControl(),
+      raisonSociale:new FormControl(),
+      siret:new FormControl()
+    
   })
-  })
-    })
-  })
-   this.formOther = new FormGroup({
-    privilege: new FormControl(),
-    idUser : new FormControl(),
-    prenom: new FormControl(),
-    nom: new FormControl(),
-    login: new FormControl(),
-    mail: new FormControl(),
-    password: new FormControl(),
-    rue: new FormControl(),
-    ville: new FormControl(),
-    département: new FormControl(),
-    raisonSociale: new FormControl(),
-    siret: new FormControl()
-   })
 
-  
   }
-
-  ajoutCommercant(){
-    this.is.ajoutCommercant(this.formUser.value).subscribe(()=>this.getCommercant())
-  }
-  ajoutProducteur(){
-    this.is.ajoutProducteur(this.formUser.value).subscribe(()=>this.getProducteur())
-  }
-  ajoutParticulier(){
-    this.is.ajoutParticulier(this.formUser.value).subscribe(()=>this.getParticulier())
-  }
-  ajoutArtisant(){
-    this.is.ajoutParticulier(this.formUser.value).subscribe(()=>this.getArtisant())
-  }
-  getParticulier(){
-    this.is.getParticulier().subscribe((data)=>{
-      this.listPart=data;
-  })
-}
-getCommercant(){
-  this.is.getCommercant().subscribe((data)=>{
-    this.listCom=data;
-})
-}  getArtisant(){
-  this.is.getArtisant().subscribe((data)=>{
-    this.listArt=data;
-})
-}
- getProducteur(){
-  this.is.getProducteur().subscribe((data)=>{
-    this.listProd=data;
-})
-}
 //________________________________________________FONCTIONS DU FORM_______________________________________________________
 
 
-
+/*Fonction d'inscription */
  inscription() {
-  console.log(this.formUser.value.privilege)
-  if (this.formUser.value.privilege == 3) { 
-    this.BoolAutres  = false;
-  }else {
-    this.BoolAutres = true;   }
-   }
+  console.log(this.formUser.value)
 
+  var test = this.formUser.value.privilege
+  if (test == 1){
+    console.log("producteur");
+    this.is.ajoutProducteur(this.formUser.value).subscribe(()=>console.log("Producteur ajouté"))}
+  else if (test == 2){
+    console.log("Artisant");
+    this.is.ajoutArtisant(this.formUser.value).subscribe(()=>console.log("Artisant ajouté"))
+  }
+  else if (test == 3){
+    console.log("Particulier");
+    this.is.ajoutParticulier(this.formUser.value).subscribe(()=>console.log("Particulier ajouté!"))
+  }
+  else if (test == 4){
+    console.log("Commercant");
+    this.is.ajoutCommercant(this.formUser.value).subscribe(()=>console.log("Commerçant ajputé"))
+  }
+  else(console.log("ERREUR"))
+ }
+
+
+  /*Fonction pour afficher le reste des inputs en fonction du privilège*/
  choisis(event : any){
-   this.inscription()
-   console.log(this.formUser.value.privilege)
-  if (this.formUser.value.privilege == 1){
-  this.BooleanProd = true;
- }else if (this.formUser.value.privilege == 2){
-   this.BooleanArt = true;
- }else if (this.formUser.value.privilege == 3){
-  this.BooleanPart = true;
-}else if (this.formUser.value.privilege == 4){
-  this.BooleanCom =true;
+  var test = this.formUser.value.privilege
+  if (test == 1 || test== 2 || test== 4){
+  this.BoolAutres=true
+ }else{this.BoolAutres=false}
  }
- }
- 
-onvalide(){
-  console.log(this.formUser.value.privilege)
-if (this.BooleanProd =true){
-  this.ajoutProducteur();
-}else if(this.BooleanArt=true){
-  this.ajoutArtisant();
-}else if (this.BooleanPart=true){
-  this.ajoutParticulier();
-}else if (this.BooleanCom=true){
-  this.ajoutCommercant();
-}
-}
 
 }
 
