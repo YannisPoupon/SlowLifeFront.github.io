d467775707ab9215351504f9cab0f53bd1de5407
Anna Baiotto <anna.baiotto@gmail.com>
2020-05-14 16:30:14 +0200

    last front

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
 
-  constructor() { }
+  constructor(private Http : HttpClient) { }
+
+  ajoutFeedback(feed:any): any{
+    return this.Http.post("http://localhost:8080/addFeedback", feed);
+  }
+  deleteFeedback(id : any){
+    return this.Http.delete("http://localhost:8080/delFeedback/"+id)
+  }
+  getChoixPart(part : any){
+    return this.Http.post("http://localhost:8080/findByPart",part);
+  }
+getAllFeedback(): any{
+  return this.Http.get("http://localhost:8080/Feedbacks");
+}
+
 }
